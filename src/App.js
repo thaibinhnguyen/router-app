f221b5f0287dbe3d8d99fd2511fab30955c218d6
thaibinhnguyen <thaibinhnguyen26041996@gmail.com>
2020-06-15 10:38:40 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NavBar from "./components/navbar";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <div className="content">
+                    <Switch>
+                        <Route
+                            path="/products/:id"
+                            component={ProductDetails}
+                        />
+                        <Route
+                            path="/products"
+                            render={(props) => (
+                                <Products sortBy="newest" {...props} />
+                            )}
+                        />
+                        <Route path="/posts/:year?/:month?" component={Posts} />
+                        <Route path="/not-found" component={NotFound} />
+                        <Redirect from="/test" to="/posts" />
+                        <Route path="/admin" component={Dashboard} />
+                        <Route path="/" exact component={Home} />
+                        <Redirect to="/not-found" />
+                    </Switch>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
